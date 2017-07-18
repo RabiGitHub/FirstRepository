be8c821db692c9b565395b9eefde4e2c73706158
Rabi <rabinarayansutar@outlook.com>
2017-07-18 12:34:16 +0530

    3rd Commit - Printing the names

--- FirstGitProject/src/org/home/MainMethod.java
+++ FirstGitProject/src/org/home/MainMethod.java
@@ -4,8 +4,12 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		
-		//My Firtst Git projects
-		System.out.println("Hello, Git!!");
+		String[] names = {"Rabi", "Sumi", "Jimunu", "Soni"};
+		
+		//Print GreetNames
+		for(int i =0;i<names.length;i++){
+			System.out.println("Name is: "+names[i]);
+		}
 	}
 
 }
