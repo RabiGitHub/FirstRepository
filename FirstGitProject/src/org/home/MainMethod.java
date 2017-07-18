6827c35d2a09c8c6ba8cd573d994fd1555c5867a
Rabi2 <rabinarayansutar@outlook.com>
2017-07-18 12:51:30 +0530

    Added to more names and a print statement

--- FirstGitProject/src/org/home/MainMethod.java
+++ FirstGitProject/src/org/home/MainMethod.java
@@ -4,11 +4,12 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		
-		String[] names = {"Rabi", "Sumi", "Jimunu", "Soni"};
+		String[] names = {"Rabi", "Sumi", "Jimunu", "Soni","Lula", "Mami"};
 		
 		//Print GreetNames
 		for(int i =0;i<names.length;i++){
 			System.out.println("Name is: "+names[i]);
+			System.out.println("How R U doing Today");
 		}
 	}
 
