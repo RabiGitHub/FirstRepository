8e3d7fed205c91521f62371b22675e18abbb8a6f
Rabi <rabinarayansutar@outlook.com>
2017-07-18 12:46:40 +0530

    added Advanced For Loop

--- FirstGitProject/src/org/home/MainMethod.java
+++ FirstGitProject/src/org/home/MainMethod.java
@@ -6,9 +6,9 @@ public class MainMethod {
 		
 		String[] names = {"Rabi", "Sumi", "Jimunu", "Soni"};
 		
-		//Print GreetNames
-		for(int i =0;i<names.length;i++){
-			System.out.println("Name is: "+names[i]);
+		//Print GreetNames foir all names
+		for(String name: names){
+			System.out.println("Name is: "+name);
 		}
 	}
 
